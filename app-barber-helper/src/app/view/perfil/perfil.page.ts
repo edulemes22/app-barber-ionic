c0a5dc93b1aaf8728aa51653df51bc1e122221bd
Eduardo <eduardolemes.2003@gmail.com>
2023-09-19 21:21:56 -0300

    Iniciado tela de perfil

--- app-barber-helper/src/app/view/perfil/perfil.page.ts
+++ app-barber-helper/src/app/view/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReservaTeste } from 'src/app/model/reserva-teste.model';
 
 @Component({
   selector: 'app-perfil',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
 export class PerfilPage {
 
   constructor() {}
+
+  visualizar:boolean = true;
+
+  item:ReservaTeste = new ReservaTeste;
+
+  ngOnInit() {
+
+    this.item.funcionario = "Cleiton Faria";
+    this.item.horario = "13/09/23 - 15:30";
+    this.item.status = "Finalizado";
+    
+  }
+
+  editar() {
+
+    this.visualizar = false;
+
+  }
+
+  salvar() {
+
+    this.visualizar = true;
+
+  }
 
 }
